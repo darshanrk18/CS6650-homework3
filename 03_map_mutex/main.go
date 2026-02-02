b28a83379e772a9199eeccbc9688fb8e4fe6fe6e
Darshan Ravindra Konnur <konnur.d@northeastern.edu>
2026-02-02 09:57:53 -0500

    feat: map mutex

--- 03_map_mutex/main.go
+++ 03_map_mutex/main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type SafeMap struct {
+	mu sync.Mutex
+	m  map[int]int
+}
+
+func main() {
+	s := SafeMap{m: make(map[int]int)}
+	var wg sync.WaitGroup
+
+	const goroutines = 50
+	const perG = 1000
+
+	start := time.Now()
+
+	wg.Add(goroutines)
+	for g := 0; g < goroutines; g++ {
+		go func(g int) {
+			defer wg.Done()
+			for i := 0; i < perG; i++ {
+				key := g*perG + i
+				s.mu.Lock()
+				s.m[key] = i
+				s.mu.Unlock()
+			}
+		}(g)
+	}
+
+	wg.Wait()
+	elapsed := time.Since(start)
+
+	fmt.Println("len(m):", len(s.m))
+	fmt.Println("time  :", elapsed)
+}
